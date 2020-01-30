4035b3c8c93113d89a7eae2787401094471bb4db
Jesus Dario <jesusmarencodev@gmail.com>
2020-01-29 20:54:02 -0500

    making http POST request to save a sale

--- src/components/user/Store.js
+++ src/components/user/Store.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import axios from 'axios';
 import Global from '../../Global';
 import Product from './Product';
-import Order from './Order';
+
 import '../../App';
 
 export default class Store extends Component {
@@ -38,13 +38,56 @@ export default class Store extends Component {
 				...this.state.items,
 				items,
 			})
-			
 		}
+	}
+	//calculating the order total
+	total = (items) => {
+		let total = 0;
+
+		for (let index = 0; index < items.length; index++) {
+			total += items[index].product.price * items[index].product.quantity;
+			if (index === items.length - 1) {
+				return total;
+			}
+		}
+	};
+	//delete items 
+	delete = (id) => () => {
+		let arrayItems = this.state.items;
+		let items = arrayItems.filter(item => item.product._id !== id);
+		this.setState({
+			...this.state,
+			items
+		})
+		
+	}
+	//save sale
+	save = () => {
+		let arrayItems = this.state.items;
+		let items  = [];
+		let product = {
+			items : []
+		}
+ 		for (let index = 0; index < arrayItems.length; index++) {
+			product.items.push(arrayItems[index].product)
+			product.items[index].product = arrayItems[index].product._id
+
+			if(index === arrayItems.length -1){
+				console.log(product)
+				axios.post(`${this.url}/sale/save`, product)
+					 .then(resp => {
+						 console.log(resp)
+					 })
+					 .catch(err => console.log(err))
+			}
+		} 
+		
 	}
 
 
 	render() {
 		let products = this.state.products;
+		let total = this.total(this.state.items);
 
 		return (	
 			<div className="container-fluid text-justify">
@@ -63,7 +106,51 @@ export default class Store extends Component {
 							})}
 						</div>
 						<div className="col-12 col-md-4 p-2">
-							<Order items={this.state.items}/>
+							{/* <Order items={this.state.items}/> */}
+							<div className="p-2 card">
+								<strong>
+									<p style={{ fontSize: '12px' }}>
+										Items added:{' '}
+										<span className="font-weight-normal">
+											In this section the items you want to buy will be added.
+										</span>
+										<span> Remember your shipment is free.</span>
+									</p>
+								</strong>
+								<hr className="m-0" />
+								<div className="py-2">
+									<p className="font-weight-bold text-success">
+										Total({`${this.state.items.length} items`}):${total}
+									</p>
+									<ul className="pl-0">
+										{this.state.items.map((item, index) => {
+											return (
+												<Fragment  key={item.product._id}>
+													<li className="row d-flex  about" onCompositionUpdate={this.prueba}>
+														<div className="col-9 mr-0">
+															<p className="col-9 font-weight-bold mr-0">{item.product.name}</p>
+														</div>
+														<div className="col-2 d-flex text-center align-items-center justify-content-center">
+															<button  onClick={this.delete(item.product._id)} className="btn btn-danger col-2 m-auto">-</button>
+														</div>
+														<hr/>
+														<div className="col-12">
+															<p className="pl-3 mb-0">Unit Price: ${item.product.price}</p>
+															<p className="pl-3 mb-0">Units: {item.product.quantity}</p>
+															<p className="pl-3 mb-0">Total item: ${item.product.price * item.product.quantity}</p>
+														</div>
+													</li>
+													<hr/>
+												</Fragment>
+											);
+										})}
+									</ul>
+
+									<button onClick={this.save} className="btn btn-warning btn-block">
+										Proceed to checkout
+									</button>
+								</div>
+							</div>
 						</div>
 					</div>
 				}
