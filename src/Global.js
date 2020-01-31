4f9380eb64a786792067545c9fa8cb1102207c00
Jesus Marenco <jesusdario10@hotmail.com>
2020-01-31 11:47:06 -0500

    change config Global url to local

--- src/Global.js
+++ src/Global.js
@@ -1,6 +1,6 @@
 const Global = {
-    url :'http://167.172.212.145:3001'
-    ////url :'http://localhost:3001'
+    //url :'http://167.172.212.145:3001'
+    url :'http://localhost:3001'
 };
 
 export default Global;
